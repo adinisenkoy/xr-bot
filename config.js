280cd1f4021e4cfb778805b2bfc658fdbed929d3
Hey Türkiye <138398712+heyturkiye204@users.noreply.github.com>
2024-01-21 00:03:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   "prefix": "!",
   "owner": "718374283642011728",
-  "token": "MTEyMjg0NDg2OTQ4MDY4MTU5NA.GmdjRI.qXPYN4tntnwmhTWtXS15yxWIaTrN95jxMXM2hU"
+  "token": "MTEyMjg0NDg2OTQ4MDY4MTU5NA.GmxgEd.v4XRhx3loHoIGOFWF6clwiWiTLTI8-SI2mXa7Q"
 }
