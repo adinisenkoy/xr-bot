7ff4701f6f915eea3bcb1761b61a57dc1c5b23c5
Hey Türkiye <138398712+heyturkiye204@users.noreply.github.com>
2024-01-21 00:02:22 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-    let activities = [ `21.01.2024 21:25'e kadar aktif olacak mıyım göricez bakıcaz sonuca` ], i = 0;
+    let activities = [ `aktifim, seni` ], i = 0;
     setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
 }};
