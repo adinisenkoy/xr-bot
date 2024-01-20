351fa5a0b8875f3eac2e849d2223613cc4bfcd2a
Hey Türkiye <138398712+heyturkiye204@users.noreply.github.com>
2024-01-20 21:19:22 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,28 +1,8 @@
-const { ActivityType, Presence } = require("discord.js");
-
+const { ActivityType } = require("discord.js")
 module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-        let activities = [`Adını Sen Koy Dizi Müzikleri`], i = 0;
-
-        setInterval(() => {
-            const now = new Date();
-            const utcPlus3Hour = new Date(now.getTime() + 3 * 60 * 60 * 1000); // UTC+3
-
-            if (utcPlus3Hour.getHours() >= 22 || utcPlus3Hour.getHours() < 8) {
-                // Akşam 22:00'den sabah 8:00'e kadar Rahatsız etmeyin durumu
-                client.user.setPresence({
-                    activity: { name: 'Rahatsız etmeyin', type: ActivityType.DND },
-                    status: 'dnd',
-                });
-            } else {
-                // Diğer zamanlarda Boşta durumu
-                client.user.setPresence({
-                    activity: { name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening },
-                    status: 'idle',
-                });
-            }
-        }, 22000);
-    },
-};
+	name: 'ready',
+	once: true,
+	execute(client) {
+    let activities = [ `21.01.2024 21:25'e kadar aktif olacak mıyım göricez bakıcaz sonuca` ], i = 0;
+    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
+}};
