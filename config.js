fae784417c49863aef3621e431985c4ab28ed14c
Hey Türkiye <138398712+heyturkiye204@users.noreply.github.com>
2024-01-20 19:39:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "prefix": "",
-  "owner": "KULLANICI ID",
-  "token": "TOKEN GİR"
+  "prefix": "!",
+  "owner": "718374283642011728",
+  "token": "MTEyMjg0NDg2OTQ4MDY4MTU5NA.GmdjRI.qXPYN4tntnwmhTWtXS15yxWIaTrN95jxMXM2hU"
 }
